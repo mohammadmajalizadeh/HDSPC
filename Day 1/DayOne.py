a2ef349af3066ec0cc6147be24f6d8b80d91e4d1
mohammadmajalizadeh <mohammadun1999@gmail.com>
2024-08-21 14:36:42 +0400

    Band generator for Day one

---
+++ Day 1/DayOne.py
@@ -0,0 +1,11 @@
+print("Welcome to the Band Name Generator\n")
+
+birthplace = "what's the name of the city you grew up in?\n"
+petName = "What's your petname?\n"
+
+birthin = input(birthplace)
+petin = input(petName)
+
+BandName = birthin +" "+petin
+
+print("your band name could be" + " " + BandName)
